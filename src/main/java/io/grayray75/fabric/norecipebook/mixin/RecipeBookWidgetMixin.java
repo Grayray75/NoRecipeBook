70076b06dbe5d478d37211a9151639533d78ca99
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2021-06-17 10:35:10 +0200

    Fix a crash when opening the creative inventory

---
+++ src/main/java/io/grayray75/fabric/norecipebook/mixin/RecipeBookWidgetMixin.java
@@ -0,0 +1,29 @@
+package io.grayray75.fabric.norecipebook.mixin;
+
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.screen.AbstractRecipeScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RecipeBookWidget.class)
+public abstract class RecipeBookWidgetMixin {
+    @Shadow
+    protected AbstractRecipeScreenHandler<?> craftingScreenHandler;
+    private boolean initialized = false;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    public void init(CallbackInfo ci) {
+        this.initialized = true;
+    }
+
+    @Inject(method = "isOpen", at = @At("HEAD"), cancellable = true)
+    public void isOpen(CallbackInfoReturnable<Boolean> info) {
+        if (!initialized || craftingScreenHandler == null) {
+            info.setReturnValue(false);
+        }
+    }
+}
